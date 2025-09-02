1dd17097e19f4d6fd47024344a0ce787d3a2fdac
Maciej Krüger <mkrueger@mgit.at>
2025-09-02 12:40:17 +0200

    Change clean up oldest allowed to 6 hours, default ci timeout

--- .github/clear-up-hetzner.py
+++ .github/clear-up-hetzner.py
@@ -11,20 +11,20 @@ assert client.ssh_keys.get_by_name(MAGIC_SSH_KEY), (
     f"This hetzner cloud project does not have the magic ssh-key named '{MAGIC_SSH_KEY}', refusing to delete anything..."
 )
 
-# Two days ago from now in UTC
-two_days_ago = datetime.now(timezone.utc) - timedelta(days=2)
+# 6 hours ago from now in UTC - github runner timeout
+oldest_allowed = datetime.now(timezone.utc) - timedelta(hours=6)
 now = datetime.now(timezone.utc)
 
-print("deleting servers and keys older than 2 days...")
+print("deleting servers and keys older than {timeago.format(oldest_allowed)}...")
 
 for server in client.servers.get_all():
-    if server.created < two_days_ago:
+    if server.created < oldest_allowed:
         print(f"Server {server.name} is being deleted, it was created {timeago.format(server.created, now)}...")
         client.servers.delete(server)
 
 for ssh_key in client.ssh_keys.get_all():
     if ssh_key.name == MAGIC_SSH_KEY:
         continue
-    if ssh_key.created < two_days_ago:
+    if ssh_key.created < oldest_allowed:
         print(f"SSH-Key {ssh_key.name} is being deleted, it was created {timeago.format(ssh_key.created, now)}...")
         client.ssh_keys.delete(ssh_key)
